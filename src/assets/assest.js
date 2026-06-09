79cbee50da7e4f57c1ecba670dd85a085d83187a
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-06-09 10:20:05 +0530

    New Commit

--- src/assets/assest.js
+++ src/assets/assest.js
@@ -1,11 +1,11 @@
 import ImageS1 from './ImageA/ImageS1.jpeg'
 import ImageS2 from './ImageA/ImageS2.jpeg'
 import ImageS3 from './ImageA/ImageS3.png'
-import Project1Image from './ImageA/PortfolioImage.png'
-import Project2Image from './ImageA/ImageGallary.png'
-import Project3Image from './ImageA/MidNFooDWeb.png'
-import Project4Image from './ImageA/ZenitsuWebsite.png'
-import Project5Image from './ImageA/ParklyImage.png'
+import Project1Image from './ImageA/PortfolioImage.jpeg'
+import Project2Image from './ImageA/ImageGallary.jpeg'
+import Project3Image from './ImageA/MidNFooDWeb.jpeg'
+import Project4Image from './ImageA/ZenitsuWebsite.jpeg'
+import Project5Image from './ImageA/ParklyImage.jpeg'
 
 export const Portfolio = {
     ImageS1,
